fbc523126cceca0d5645e9f74a5c60e2bc92000e
Michelle Chen <mich.chen.94@gmail.com>
2020-07-13 16:46:03 -0700

    use sys.argv to pass input file in command line terminal.

--- wordcount.py
+++ wordcount.py
@@ -1,30 +1,40 @@
- # counts how many times each space-separated word occurs in that file
+# counts how many times each space-separated word occurs in that file
 # print counts to the screen " word count"
 
-def wordcount(filename):
+import sys
+
+
+def wordcount():
     """ prints how many times each space-separated word occurs in a file """
 
-    file = open(filename)
+    filename = sys.argv[1]
+
     # open filename and store object in file (contains each line)
 
     word_count = {}
     punctuation = (',', '.', '?', '!')
 
-    for line in file:
+    def unpunctuate(word):
+        
+        word_letters = []
+        if word.endswith(punctuation):
+            word_letters = []
+            for letter in word:
+                if letter not in punctuation:
+                    word_letters.append(letter)
+            word = ''.join(word_letters)
+        return word
+
+    for line in open(filename):
         words = line.rstrip().split(' ')
         for word in words:
             word = word.lower()
-            if word.endswith(punctuation):
-                word_letters = []
-                for letter in word:
-                    if letter not in punctuation:
-                        word_letters.append(letter)
-                word = ''.join(word_letters)
+            word = unpunctuate(word)
                         
             word_count[word] = word_count.get(word, 0) + 1
 
     for key, value in word_count.items():
         print(key, value)
 
-wordcount('test.txt')
+wordcount()
 
